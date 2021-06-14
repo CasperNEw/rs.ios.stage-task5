ac4cf4402fe37d15d5d00716a4c7c0ccf8ff729e
Dmitriy Konstantinov <dkonstantinov@i-neti.ru>
2021-06-14 11:54:58 +0300

    feat: added implementation for 1-3 exercise

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -2,7 +2,22 @@ import Foundation
 
 class StockMaximize {
 
+    private var currentProfit = 0
+
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        currentProfit = 0
+        var prices = prices
+        calculateGrowth(of: &prices, startIndex: 0)
+        return currentProfit
+    }
+
+    private func calculateGrowth(of array: inout Array<Int>, startIndex: Int) {
+        let currentSlice = array[startIndex...]
+        guard let vertex = currentSlice.max(),
+              let endIndex = currentSlice.firstIndex(of: vertex) else { return }
+
+        currentProfit += array[startIndex..<endIndex].reduce(0) { $0 + (vertex - $1) }
+        guard array.count > endIndex + 1 else { return }
+        calculateGrowth(of: &array, startIndex: endIndex + 1)
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
@@ -3,20 +3,50 @@ import Foundation
 public typealias Supply = (weight: Int, value: Int)
 
 public final class Knapsack {
+
     let maxWeight: Int
     let drinks: [Supply]
     let foods: [Supply]
     var maxKilometers: Int {
         findMaxKilometres()
     }
-    
+
     init(_ maxWeight: Int, _ foods: [Supply], _ drinks: [Supply]) {
         self.maxWeight = maxWeight
         self.drinks = drinks
         self.foods = foods
     }
-    
+
     func findMaxKilometres() -> Int {
-        return -1
+        let drinkDistance = getDistanceArray(for: drinks, with: maxWeight)
+        let foodDistance = getDistanceArray(for: foods, with: maxWeight)
+
+        var maxDistance = 0
+        for i in 0...maxWeight {
+            let distance = min(drinkDistance[i], foodDistance[maxWeight - i])
+            if distance > maxDistance {
+                maxDistance = distance
+            }
+        }
+        return maxDistance
+    }
+
+    private func getDistanceArray(for items: [Supply], with weight: Int) -> [Int] {
+        let count = items.count
+        var matrix = Array(repeating: Array(repeating: 0,count: weight + 1), count: count + 1)
+
+        for i in 0..<(count + 1) {
+            for j in 0..<(weight + 1) {
+                if i == 0 || j == 0 {
+                    matrix[i][j] = 0
+                } else if items[i - 1].weight <= j {
+                    matrix[i][j] = max(items[i - 1].value + matrix[i - 1][j - items[i - 1].weight],
+                                       matrix[i - 1][j])
+                } else {
+                    matrix[i][j] = matrix[i - 1][j]
+                }
+            }
+        }
+        return matrix[count]
     }
 }
